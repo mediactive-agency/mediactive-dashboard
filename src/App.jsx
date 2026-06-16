3872f2d34002961739d84cb958cecf31a459526a
mediactive-agency <fritsch.krystof@gmail.com>
2026-06-16 14:11:50 +0200

    feat: pass vslMode to OnboardingTour

--- src/App.jsx
+++ src/App.jsx
@@ -138,6 +138,7 @@ export default function App() {
           onClose={() => setTourOpen(false)}
           onNav={(p) => { setPage(p); setTourOpen(false) }}
           isMobile={isMobile}
+          vslMode={config?.vslMode ?? false}
         />
       )}
     </div>
